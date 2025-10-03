29d12f6d1ab9c67dd6367ad723073d11bb23b776
wasif_manzoor <wasifahmad679@gmail.com>
2025-10-03 20:26:59 +0530

    js

---
+++ JS TUTORIAL/1.js
@@ -0,0 +1,6 @@
+// document.getElementById("h").innerHTML = 'WASIF'
+// alert("Hi")
+document.write("wasiffff") // Dynamically adding something
+
+console.log("HEllo world"); // Will be seen in console of web page
+alert(5 + 6);
