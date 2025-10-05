eeae58f73dc22c58df5fda3401c5814cd24d3dbe
wasif_manzoor <wasifahmad679@gmail.com>
2025-10-05 22:32:49 +0530

    :)

--- JS TUTORIAL/ARRAY/ArrayLoop.js
+++ JS TUTORIAL/ARRAY/ArrayLoop.js
@@ -1,11 +1,9 @@
-let books = ["Math", "Physics", "Bio", "Chemistry"];
+// Printing each book using for loop
 
+let books = ["Math", "Physics", "Bio", "Chemistry"];
 let bookLength = books.length; // length = 4
 
 for (let i = 0; i < bookLength; i++) { // using less than (<) bookLength because indexing starts from 0 and length starts from 1
 
-
-
-
     console.log(`Element ${i+1} is ${books[i]}\n`)
-}
+};
