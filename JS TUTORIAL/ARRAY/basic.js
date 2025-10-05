ac63c19a31b3db15e749c2a8f7417f19dfcd8a54
wasif_manzoor <wasifahmad679@gmail.com>
2025-10-05 17:54:11 +0530

    array

---
+++ JS TUTORIAL/ARRAY/basic.js
@@ -0,0 +1,15 @@
+// Two ways to make an declare array 
+
+let books = ["Math", "Physics", "Bio", "Chemistry", "12"];
+
+// let book = new Array("Math", "Physics", "Bio", "Chemistry");
+
+// print 1 element of an array
+console.log(books[1])
+
+//  print whole array
+console.log(books)
+
+// Update array 
+books[1] = "english"
+console.log(books)
