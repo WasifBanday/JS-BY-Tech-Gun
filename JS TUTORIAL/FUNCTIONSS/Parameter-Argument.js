b83071f4aa106b2cab656d55af427ca98d93c888
wasif_manzoor <wasifahmad679@gmail.com>
2025-10-09 16:28:45 +0530

    :)

--- JS TUTORIAL/FUNCTIONSS/Parameter-Argument.js
+++ JS TUTORIAL/FUNCTIONSS/Parameter-Argument.js
@@ -5,4 +5,4 @@ function MulTable(num) { // Num :- Parameter (variable defined inside function)
     console.log(`\n`)
 }
 MulTable(2); // (2) : Argument (value assign to the variable defined in function)
-MulTable(5);
+MulTable(5) // (5) : Argument (value assign to the variable defined in function)
