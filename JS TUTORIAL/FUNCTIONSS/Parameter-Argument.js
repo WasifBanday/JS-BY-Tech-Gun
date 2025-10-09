3498b65ae1fa76d964b31a7a91b0e91d215d686e
wasif_manzoor <wasifahmad679@gmail.com>
2025-10-09 16:33:17 +0530

    two nums

--- JS TUTORIAL/FUNCTIONSS/Parameter-Argument.js
+++ JS TUTORIAL/FUNCTIONSS/Parameter-Argument.js
@@ -6,3 +6,12 @@ function MulTable(num) { // Num :- Parameter (variable defined inside function)
 }
 MulTable(2); // (2) : Argument (value assign to the variable defined in function)
 MulTable(5) // (5) : Argument (value assign to the variable defined in function)
+
+
+
+// Lets pass two Arguments
+
+function add(num1, num2) { // Two parameters defined here / or we can say two variables defined ....
+    console.log(num1 + num2)
+}
+add(5, 7) // 5 chala gaya num1 mai .. aur 7 chala gaya num2 mai
