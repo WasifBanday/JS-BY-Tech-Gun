c8ef70d1677957e762fbb29e8081e4ddf5b05cd5
wasif_manzoor <wasifahmad679@gmail.com>
2025-10-10 02:52:05 +0530

    Math Functions

---
+++ JS TUTORIAL/OBJECT/Math.js
@@ -0,0 +1,44 @@
+// Math Object : there are various math values which are already defined
+console.log(Math.PI) // value of pi
+
+
+
+// There are various functions of math which are defined 
+
+// Round-off function   
+let a = Math.round(4.3)
+let b = Math.round(4.8)
+console.log(a)
+console.log(b)
+
+
+// Ceiling function : Hamesha uppar wala dekha ta hai
+let c = Math.ceil(4.9) // 5 
+let d = Math.ceil(4.1) // also 5 
+console.log(c)
+console.log(d)
+
+
+// floor function : Hamesha neechey wala dekhana hai
+let e = Math.floor(4.1) // 4 
+let f = Math.floor(4.9) // also 4
+console.log(e)
+console.log(f)
+
+
+// Power function ...
+let g = Math.pow(2, 3) // means 2 to the power 3
+console.log(g)
+
+// Square Root function 
+let h = Math.sqrt(100)
+console.log(h)
+
+
+// Minimum function :  Numbers passed minimum will be output
+let i = Math.min(2, 3, 4, -3, 5, 3, 2, 3333)
+console.log(i)
+
+// Maximum function : Numbers passed maximum will be output 
+let j = Math.max(34, 43, -223, 23, 4, 3, 2, 32, 3, 34, 2, 34, 390, 42)
+console.log(j)
