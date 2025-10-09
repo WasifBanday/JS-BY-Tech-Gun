c630fdedc6f8c6eb319a417f4ba68b220c45d105
wasif_manzoor <wasifahmad679@gmail.com>
2025-10-09 23:39:16 +0530

    ..

--- JS TUTORIAL/OBJECT/ForInLoop.js
+++ JS TUTORIAL/OBJECT/ForInLoop.js
@@ -7,6 +7,8 @@ let person = {
     height: 5.9,
 };
 
+
+// loop k andr 'Array' notation use karni hai properties ki value ko print karney k lia  ,, dot notation kaam nahi kareiga
 for (let key in person) { // key k andrr sarie properties atie hai abri bari se 
     console.log(key + " " + person[key]) // fir yehaan unhi ko print / console krtey hai,, person[key] se properties ki value print hoti hai
 
