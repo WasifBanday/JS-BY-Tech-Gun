547f864cd980aeb45cc2195b228e316006b6effe
wasif_manzoor <wasifahmad679@gmail.com>
2025-10-10 18:08:31 +0530

    Set-Method

--- JS TUTORIAL/OBJECT/Set-Methods.js
+++ JS TUTORIAL/OBJECT/Set-Methods.js
@@ -1,1 +1,27 @@
+// How to change any element in Date 
+let x = new Date("june 21,2023 10:12:33")
 
+// Changed year from 2023 to 2024
+x.setFullYear(2024)
+console.log(x)
+
+// Changed year from 2023 to 2024
+x.setMontswh(12)
+console.log(x)
+
+// Changed year from 2023 to 2024
+x.setFullYear(2024)
+console.log(x)
+
+// Changed year from 2023 to 2024
+x.setFullYear(2024)
+console.log(x)
+
+
+// Changed year from 2023 to 2024
+x.setFullYear(2024)
+console.log(x)
+
+// Changed year from 2023 to 2024
+x.setFullYear(2024)
+console.log(x)
