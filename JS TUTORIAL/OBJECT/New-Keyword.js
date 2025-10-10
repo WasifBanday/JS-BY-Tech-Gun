8ff5d24ce98bf7404e764c3d61b825db9ab1fcf3
wasif_manzoor <wasifahmad679@gmail.com>
2025-10-10 21:01:58 +0530

    New Keywaord

---
+++ JS TUTORIAL/OBJECT/New-Keyword.js
@@ -0,0 +1,12 @@
+// Now lets see how we make object useing 'new' Keyword 
+// By simple 
+// let person = {
+//     name: 'wasif',
+//     age: '20'
+// }
+// console.log(person.name)
+
+let person = new Object(); // object ban chuuka hai
+person.name = 'Wasif Ahmad Banday' // Property
+person.age = 20 // Property
+console.log(person.age)
