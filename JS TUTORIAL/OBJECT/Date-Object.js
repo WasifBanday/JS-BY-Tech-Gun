93b73e6a6d6d79d8e70a4469b391bb5173a20b46
wasif_manzoor <wasifahmad679@gmail.com>
2025-10-11 02:35:05 +0530

    Nested

--- JS TUTORIAL/OBJECT/Date-Object.js
+++ JS TUTORIAL/OBJECT/Date-Object.js
@@ -9,4 +9,3 @@ console.log(dt) // Month starts from 0 in J-S ( 0--> january)
 // We can pass parameters as a string as well
 let str = new Date("june 12,2023 11:12:22")
 console.log(str)
-console.log(str.getTime()) // The computer stores mili-seconds from 1970 to the date we provided .
